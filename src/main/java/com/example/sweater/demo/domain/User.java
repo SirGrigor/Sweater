5964235ffeec6a496e0fee811d49a1456aefe07f
grigorjev.ilja@gmail.com <werbatim58803110>
2020-09-24 08:55:25 +0300

    1. Added email authorization 2. Identification 3. Bootstrap frontend updated

--- src/main/java/com/example/sweater/demo/domain/User.java
+++ src/main/java/com/example/sweater/demo/domain/User.java
@@ -16,7 +16,25 @@ public class User implements UserDetails {
     private String username;
     private String password;
     private boolean active;
+    private String email;
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getActivationCode() {
+        return activationCode;
+    }
+
+    public void setActivationCode(String activationCode) {
+        this.activationCode = activationCode;
+    }
+
+    private String activationCode;
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
     @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.STRING)
